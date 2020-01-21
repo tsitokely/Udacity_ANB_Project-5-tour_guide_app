f0099673ca8c76b712e6d88f12fb286810f27bbe
Toetra <tsitohaina.toetra.rak@gmail.com>
2020-01-21 13:05:29 +0300

    Add data to gasoline fragment

--- app/src/main/java/com/example/android/project5_tourguideapp/GasolineFragment.java
+++ app/src/main/java/com/example/android/project5_tourguideapp/GasolineFragment.java
@@ -1,6 +1,41 @@
 package com.example.android.project5_tourguideapp;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+
 import androidx.fragment.app.Fragment;
 
+import java.util.ArrayList;
+
 public class GasolineFragment extends Fragment {
+
+
+    public GasolineFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.location_list, container, false);
+        final ArrayList<LocationTour> gasoline = new ArrayList<>();
+        gasoline.add(new LocationTour("Total Villa Plessis",
+                "Bd. Augagneur"));
+        gasoline.add(new LocationTour("Total Vatsy",
+                "RN2 Bis"));
+        gasoline.add(new LocationTour("Jovenna Jacaranda",
+                "Bd. Augagneur"));
+        gasoline.add(new LocationTour("Shell Pangalana",
+                "RN5"));
+        gasoline.add(new LocationTour("Shell Gare Routière",
+                "Bd. D’andovoranto"));
+        LocationAdapter itemsAdapter = new LocationAdapter(getActivity(), gasoline,
+                R.color.Gasoline_location);
+        ListView listview = rootView.findViewById(R.id.list);
+        listview.setAdapter(itemsAdapter);
+        return rootView;
+    }
 }
