9effeade130ae82f22d78c871d8bd1cd0346cb2e
Toetra <tsitohaina.toetra.rak@gmail.com>
2020-01-21 12:50:50 +0300

    Change resources name to match standard

--- app/src/main/java/com/example/android/project5_tourguideapp/BankFragment.java
+++ app/src/main/java/com/example/android/project5_tourguideapp/BankFragment.java
@@ -11,7 +11,7 @@ import androidx.fragment.app.Fragment;
 import java.util.ArrayList;
 
 public class BankFragment extends Fragment {
-    
+
     public BankFragment() {
         // Required empty public constructor
     }
@@ -20,21 +20,19 @@ public class BankFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.location_list, container, false);
-        /** Array list of bank - Data declaration **/
         final ArrayList<LocationTour> bank = new ArrayList<>();
         bank.add(new LocationTour("BFV-SG", "Av. Independence",
-                R.drawable.BFV_Av_Indep));
+                R.drawable.bfv_av_indep));
         bank.add(new LocationTour("BFV-SG", "Tanambao V",
-                R.drawable.BFV_Tanambao_5));
+                R.drawable.bfv_tanambao_5));
         bank.add(new LocationTour("B.O.A", "Bd. Augagneur",
-                R.drawable.BOA_Bd_Augagneur));
+                R.drawable.boa_bd_augagneur));
         bank.add(new LocationTour("B.O.A", "Rue du Commerce",
-                R.drawable.BOA_Rue_Commerce));
+                R.drawable.boa_rue_commerce));
         bank.add(new LocationTour("B.M.O.I", "Bd. Joffre",
-                R.drawable.BMOI_Bd_Joffre));
+                R.drawable.bmoi_bd_joffre));
         bank.add(new LocationTour("B.M.O.I", "Bd. Augagneur",
-                R.drawable.BMOI_Bd_Augagneur));
-        /** Instantiate the custom location adapter to provide the theme color **/
+                R.drawable.bmoi_bd_augagneur));
         LocationAdapter itemsAdapter = new LocationAdapter(getActivity(), bank,
                 R.color.bank_location);
         ListView listview = rootView.findViewById(R.id.list);
